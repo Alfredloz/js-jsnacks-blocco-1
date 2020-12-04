76824d1b28cc41b5996deb6ae71bc12944ac689f
Alfredo <alfredogregorio.lozano@gmail.com>
2020-12-04 16:59:05 +0100

    esercizio 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,58 +3,58 @@ $(function(){
 // Crea 10 oggetti che rappresentano una zucchina,
 // indicando per ognuna varietà, peso e lunghezza.
 // Calcola quanto pesano tutte le zucchine.
-// var zucchina = [
-//     {
-//         varieta: 'Calabra',
-//         peso: 3,
-//         lunghezza:10
-//     },
-//     {
-//         varieta: 'Fiorentina',
-//         peso: 4,
-//         lunghezza: 8,
-//     },
-//     {
-//         varieta:'Emiliana',
-//         peso: 4,
-//         lunghezza: 12
-//     },
-//     {
-//         varieta: 'Spagnola',
-//         peso: 7,
-//         lunghezza: 6
-//     },
-//     {
-//         varieta: 'Brasiliana',
-//         peso: 15,
-//         lunghezza: 15,
-//     },
-//     {
-//         varieta: 'Giapponese',
-//         peso: 10,
-//         lunghezza: 8
-//     },
-//     {
-//         varieta: 'Georgiana',
-//         peso: 3,
-//         lunghezza: 11
-//     },
-//     {
-//         varieta: 'Faenza',
-//         peso: 14.2,
-//         lunghezza: 8.3
-//     },
-//     {
-//         varieta: 'Francese',
-//         peso: 9.9,
-//         lunghezza: 9
-//     },
-//     {
-//         varieta: 'Croata',
-//         peso: 4.4,
-//         lunghezza: 5
-//     },
-// ];
+var zucchina = [
+    {
+        varieta: 'Calabra',
+        peso: 3,
+        lunghezza:10
+    },
+    {
+        varieta: 'Fiorentina',
+        peso: 4,
+        lunghezza: 8,
+    },
+    {
+        varieta:'Emiliana',
+        peso: 4,
+        lunghezza: 17
+    },
+    {
+        varieta: 'Spagnola',
+        peso: 7,
+        lunghezza: 6
+    },
+    {
+        varieta: 'Brasiliana',
+        peso: 15,
+        lunghezza: 15,
+    },
+    {
+        varieta: 'Giapponese',
+        peso: 10,
+        lunghezza: 8
+    },
+    {
+        varieta: 'Georgiana',
+        peso: 3,
+        lunghezza: 21
+    },
+    {
+        varieta: 'Faenza',
+        peso: 14.2,
+        lunghezza: 8.3
+    },
+    {
+        varieta: 'Francese',
+        peso: 9.9,
+        lunghezza: 9
+    },
+    {
+        varieta: 'Croata',
+        peso: 4.4,
+        lunghezza: 5
+    },
+];
 // var sum = 0;
 // zucchina.forEach(function(zucchine) {  
 //     var pesoZucchina = zucchine.peso;
@@ -68,18 +68,38 @@ $(function(){
 // argomento e la ritorni girata (es. Ciao -> oaiC)
 /**
  * girare le parole
- * @param {string} string 
+ * //@param {string} string 
  */
-function paroleGirate(string) {
-    if (isNaN(string)) {
-       return string.split("").reverse().join("");
+// function paroleGirate(string) {
+//     if (isNaN(string)) {
+//        return string.split("").reverse().join("");
         
-    } else {
-        alert('questo è un numero!');
-    }
+//     } else {
+//         alert('questo è un numero!');
+//     }
     
-}
-var parolaUtente = paroleGirate(prompt('Inserisci una parola'));
-console.log(parolaUtente);
+// }
+// var parolaUtente = paroleGirate(prompt('Inserisci una parola'));
+// console.log(parolaUtente);
 
+//!snack 7
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano
+// meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi
+// di zucchine
+//TODO ATTIVARE ESERCIZIO ZUCCHINE 
+var zucchinePiccole = [];
+var zucchineGrandi = [];
+zucchina.forEach(function(zucchine){
+    var dimesions = zucchine.lunghezza;
+    if (dimesions >= 15) {
+        console.log(zucchine.lunghezza);
+        zucchineGrandi.push(zucchine)
+    } else {
+        zucchinePiccole.push(zucchine);
+    }
+});
+console.log(zucchinePiccole);
+console.log(zucchineGrandi);
 });
